5a041bf38b8782d8e925ff2c92e9eda3c12f5e04
auliaditaaa <46667623+auliaditaaa@users.noreply.github.com>
2020-04-16 19:35:50 +0700

    Add files via upload

---
+++ views/admin/dashboard_view.php
@@ -0,0 +1,48 @@
+<!-- OVERVIEW -->
+<div class="panel panel-headline">
+	<div class="panel-heading">
+		<h3 class="panel-title">Welcome to our kitchen!!</h3>
+		<p class="panel-subtitle">Healthy, Delicious and hygienic</p>
+	</div>
+	<div class="panel-body">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="metric">
+					<span class="icon"><i class="fa fa-download"></i></span>
+					<p>
+						<span class="number"><?=count($list_transaksi);?></span>
+						<span class="title">Transaksi</span>
+					</p>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="metric">
+					<span class="icon"><i class="fa fa-shopping-bag"></i></span>
+					<p>
+						<span class="number"><?=count($list_pelanggan);?></span>
+						<span class="title">Pelanggan</span>
+					</p>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="metric">
+					<span class="icon"><i class="fa fa-eye"></i></span>
+					<p>
+						<span class="number"><?=count($list_admin);?></span>
+						<span class="title">Admin</span>
+					</p>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="metric">
+					<span class="icon"><i class="fa fa-bar-chart"></i></span>
+					<p>
+						<span class="number"><?=count($list_masakan);?></span>
+						<span class="title">Masakan</span>
+					</p>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+</div>
